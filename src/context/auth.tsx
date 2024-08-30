85a9f6f0a020fe24eb71ff79f7538d28f70b1950
Nguyen Nguyen Khang[khangnguyen] <khangnguyen@nexondv.com>
2024-08-30 17:14:35 +0700

    feat: create auth context

---
+++ src/context/auth.tsx
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+
+export interface IAuthContext {
+  isAuthenticated: boolean;
+  login: () => void;
+  logout: () => void;
+}
+
+const AuthContext = React.createContext<IAuthContext | null>(null);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        login: () => setIsAuthenticated(true),
+        logout: () => setIsAuthenticated(false),
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+}
